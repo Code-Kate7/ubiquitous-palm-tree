6b453636520c2d4f05f1629f8651fa64b05f9552
Code-Kate7 <kate.ember99@gmail.com>
2021-06-08 18:43:01 +0200

    erm

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const mainRouter = require('./mainRoutes.js')
+const port = process.env.PORT || 3000
 
 app.use(mainRouter)
-app.listen(3000)
+app.listen(port)
 console.log('Express server running on port 3000')
